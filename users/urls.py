a31cec198ae813a1d7f672ca3fa4cfdb953b787a
Konstantin <balashov-92@mail.ru>
2021-03-16 14:31:13 +0300

    users can register and authenticate

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from users.views import CreateUserView
+
+urlpatterns = [
+    path('registration/', CreateUserView.as_view()),
+    path('token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+]
