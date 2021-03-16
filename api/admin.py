85da9ca6bc1064fea3daaeaf94f009932d236f3c
Konstantin <balashov-92@mail.ru>
2021-03-16 22:43:32 +0300

    added admin models for all models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Event, Track, EventTrack
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'time_limit', 'max_tracks',)
+    search_fields = ('title', 'time_limit', 'max_tracks',)
+    list_filter = ('title', 'time_limit', 'max_tracks',)
+
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'artist')
+    search_fields = ('name', 'artist')
+    list_filter = ('name', 'artist')
+
+
+@admin.register(EventTrack)
+class EventTrackAdmin(admin.ModelAdmin):
+    list_display = ('track', 'user', 'score')
+    search_fields = ('score', )
+    list_filter = ('score', )
