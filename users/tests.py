48fefc40f05abae1f3abc63d5bcd02b543f9b5f3
Konstantin <balashov-92@mail.ru>
2021-03-22 09:43:35 +0300

    created user tests

--- users/tests.py
+++ users/tests.py
@@ -5,11 +5,12 @@ from rest_framework import status
 from api.models import User
 
 
-class ApiTests(TestCase):
+class UserTests(TestCase):
 
     def setUp(self):
         self.client = Client()
         self.url_registration = '/api/v1/user/registration/'
+        self.url_token = '/api/v1/user/token/'
         self.user = User.objects.create_user(
             username="sarah", email="connor.s@skynet.com",
             password="12345qwerty"
@@ -17,14 +18,36 @@ class ApiTests(TestCase):
         self.client.login(username=self.user.username,
                           password=self.user.password)
 
-    def test_registration(self):
-        self.client.post(self.url_registration, {'username': 'Vasya',
-                                                 'password': 'admin1234'})
+    def test_registration_ok(self):
+        """Тест успешной регистрации"""
+        response = self.client.post(self.url_registration,
+                                    {'username': 'Vasya',
+                                     'password': 'admin1234'})
         user = get_object_or_404(User, username='Vasya')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(user.username, 'Vasya')
 
-    def test_registration_2(self):
+    def test_registration_false(self):
+        """Тест неуспешной регистрации. Пользователь уже существует."""
         response = self.client.post('/api/v1/user/registration/',
                                     {'username': self.user.username,
                                      'password': self.user.password})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST,
+                         'Пользователь уже существует.')
+        response1 = self.client.post('/api/v1/user/registration/',
+                                     {'username': '',
+                                      'password': self.user.password})
+        self.assertEqual(response1.status_code, status.HTTP_400_BAD_REQUEST,
+                         'username не может состоять из пробельного символа.')
+        response2 = self.client.post('/api/v1/user/registration/',
+                                     {'username': 'Robinson2',
+                                      'password': ''})
+        self.assertEqual(response2.status_code, status.HTTP_400_BAD_REQUEST,
+                         'Пароль не может быть пробельным символом.')
+        response3 = self.client.post('/api/v1/user/registration/',
+                                     {'username': 'Robinson3'})
+        self.assertEqual(response3.status_code, status.HTTP_400_BAD_REQUEST,
+                         'Не передали пароль.')
+
+
+
