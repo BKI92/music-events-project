8dc2c087b1e8ff13af87fb462a0bf60b18a32f21
Konstantin <balashov-92@mail.ru>
2021-03-24 15:13:24 +0300

    added validators

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
 from .models import Event, EventTrack, EventTrackRating, Track
@@ -13,6 +15,19 @@ class EventTrackSerializer(serializers.ModelSerializer):
     rating = serializers.SerializerMethodField('get_rating', default=0)
     serializers.SerializerMethodField('ordering_by')
 
+    # def validate(self, attrs):
+    #     event_id = attrs.get('event')
+    #     # track = self.context['track'].user
+    #     event = get_object_or_404(Event, id=event_id)
+    #     if event.max_tracks <= event.tracks.count():
+    #         raise serializers.ValidationError(
+    #             {'retry error': 'You have already voted on this track'})
+    #     if event.time_limit < timezone.now():
+    #         raise serializers.ValidationError(
+    #             {'detail': 'The time for adding tracks to this event is over.'})
+    #
+    #     return attrs
+
     class Meta:
         model = EventTrack
         fields = ('id', 'event', 'track', 'rating',)
@@ -27,11 +42,20 @@ class EventTrackSerializer(serializers.ModelSerializer):
         return 0
 
     def validate(self, attrs):
-        event = attrs.get('event').id
-        track = attrs.get('track').id
-        if EventTrack.objects.filter(event=event, track=track):
+        event_id = attrs.get('event').id
+        track_id = attrs.get('track').id
+        event = get_object_or_404(Event, id=event_id)
+        if EventTrack.objects.filter(event=event_id, track=track_id):
             raise serializers.ValidationError(
                 {'retry error': 'This track on current Event already exist.'})
+
+        if event.max_tracks <= event.tracks.count():
+            raise serializers.ValidationError(
+                {'retry error': 'The maximum number of tracks has been reached '
+                                'on this event '})
+        if event.time_limit < timezone.now():
+            raise serializers.ValidationError(
+                {'detail': 'The time for adding tracks to this event is over.'})
 
         return attrs
 
